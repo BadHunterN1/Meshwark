2c67d79cb6479cbcfe3a42b2155587848f1fde90
ahmedelshkh <ahmedelshkh6566@gmail.com>
2025-08-03 11:04:38 +0300

    add pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,8 @@
+import {BusIcon, SearchIcon} from "../assets/Images/Icons"
 export default function HomePage() {
-	return <div className="wrapper">HomePage</div>;
+	return <div className="wrapper">
+		HomePage
+		<BusIcon />
+		<SearchIcon />
+		</div>;
 }
