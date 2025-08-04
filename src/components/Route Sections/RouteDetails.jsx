0daf7cebe2612f7a474c73634555fc3c7cdb09ba
mo7ammed-eldeek <mo7ammed.osama.iam.mafia@gmail.com>
2025-08-04 12:59:13 +0300

    Resturcture folders for TripDetails

--- src/components/Route Sections/RouteDetails.jsx
+++
@@ -1,5 +0,0 @@
-import { MapPin } from "lucide-react";
-
-export default function RouteDetails() {
-	return <div className="p-6"></div>;
-}
---
+++ src/components/Trip Sections/TripDetails.jsx
@@ -0,0 +1,5 @@
+import { MapPin } from "lucide-react";
+
+export default function TripDetails() {
+  return <div className="p-6"></div>;
+}
