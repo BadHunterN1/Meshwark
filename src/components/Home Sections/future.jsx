32ace31632504c909983c6d6d2bfb0e1970d5cf1
Nadaramy-aboelenien <ndyramy4@gmail.com>
2025-08-05 00:45:52 +0300

    editing in direction

--- src/components/Home Sections/future.jsx
+++ src/components/Home Sections/future.jsx
@@ -22,7 +22,7 @@ function Future() {
      <h1 className='text-[40px] font-semibold mb-3'>لماذا تختار تطبيقنا ؟</h1>
      <p className='mb-15 font-medium text-[20px] text-[#838584]'>نقدم لك افضل تجربه نقل فى المنصوره مع ميزات حصريه تجعل رحلتك اسهل واسرع</p>
       
-     <div className='grid   md:grid-cols-3 gap-9  place-items-center mx-auto xl:px-30  '>
+     <div dir="ltr" className='grid   md:grid-cols-3 gap-9  place-items-center mx-auto xl:px-30  '>
        {futureItems.map((item, index) => (
       <div key={index} className=' h-full  w-full  bg-white py-7 px-[30px] rounded-lg shadow-md  transition delay-100 ease-in-out duration-300 hover:scale-106  hover:bg-gradient-to-b from-[#EAF4FF] to-[#eaf7ff] hover:shadow-xl group '>
        <div className={`  mx-auto text-white w-16 h-16 flex items-center justify-center mb-4 rounded-xl `} style={item.style} >
