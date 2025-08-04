70bf90563fc3d85527116bbe7648414f307d1166
mo7ammed-eldeek <mo7ammed.osama.iam.mafia@gmail.com>
2025-08-04 14:34:09 +0300

    solve layout

--- src/components/Trip Sections/TripDetails.jsx
+++ src/components/Trip Sections/TripDetails.jsx
@@ -2,22 +2,20 @@ import { MapPin } from "lucide-react";
 
 export default function TripDetails() {
   return (
-    <div className="container p-6 h-50 w-50]">
-      <section className="details">
-        <h3 className="font-semibold text-2xl leading-none p-6">
+    <section className="p-2">
+      <div className="container">
+        <h3 className="font-semibold text-2xl leading-none p-6 ">
           تفاصيل المسار خطوة بخطوة
         </h3>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg border-2 rounded-l-2xlg  p-4 bg-[var(--secondary-color)]/20 border-[var(--secondary-color)]">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
+        <div className=" rounded overflow-hidden shadow-lg border-2 rounded-l-2xlg  p-4 bg-[var(--secondary-color)]/20 border-[var(--secondary-color)]">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptat
             </p>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
