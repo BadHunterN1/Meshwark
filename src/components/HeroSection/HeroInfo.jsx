b699cbd9c08c147c765dd1f4f366926948ed654a
ahmedelshkh <ahmedelshkh6566@gmail.com>
2025-08-04 12:31:47 +0300

    add the HeroInfo section

---
+++ src/components/HeroSection/HeroInfo.jsx
@@ -0,0 +1,12 @@
+
+
+function HeroInfo() {
+    return (
+        <p className="hero-info">
+            ✨ أكثر من 1000+ طريق متاح | 🚌 تغطية شاملة للمنصورة | ⚡ نتائج فورية
+        </p>
+        
+    )
+}
+
+export default HeroInfo;
