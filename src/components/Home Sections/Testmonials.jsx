d3f9ae0db67e5b542fc36bfaf9ae082be1eea8bb
Karim Behery <karimbehery@std.mans.edu.eg>
2025-08-05 12:22:02 +0300

    edited responsiveness

--- src/components/Home Sections/Testmonials.jsx
+++ src/components/Home Sections/Testmonials.jsx
@@ -43,7 +43,7 @@ const recordsArray = [
 
 const Testmonials = () => {
     return (
-        <section className='testmonials text-center  p-8 text-white'>
+        <section className='testmonials text-center md:p-8 text-white'>
             <h2 className=" text-4xl font-bold">ماذا يقول طلاب المنصورة؟</h2>
             <p className="my-3">اراء حقيقية من طلاب جامعة المنصورة الذين يستخدمون التطبيق يوميا</p>
             <main className="container">
