94a61db3e3a1595ea15e90b3a8ae1b09598c24a1
BadHunterN1 <ahmam2004@gmail.com>
2025-08-09 17:40:59 +0300

    fixed TestmonialCard animation not working

--- src/components/Home Sections/TestmonialCard.jsx
+++ src/components/Home Sections/TestmonialCard.jsx
@@ -12,28 +12,27 @@ const TestmonialCard = ({
     index,
 }) => {
     return (
-        <MotionFadeIn
-            delay={0.6 + index * 0.2}
-            className="testmonial-card p-2 rounded-lg bg-white/30 backdrop-invert backdrop-opacity-10 my-2 mx-4"
-        >
-            <Quote
-                size={40}
-                color="#ffffff"
-                className="bg-white/30 backdrop-invert backdrop-opacity-10 p-1 rounded-lg"
-            />
-            <div className="flex justify-center yellow p-5 stars">
-                <Star fill="yellow" />
-                <Star fill="yellow" />
-                <Star fill="yellow" />
-                <Star fill="yellow" />
-                <Star fill="yellow" />
+        <MotionFadeIn delay={0.6 + index * 0.2}>
+            <div className="testmonial-card p-2 rounded-lg bg-white/30 backdrop-invert backdrop-opacity-10 my-2 mx-4">
+                <Quote
+                    size={40}
+                    color="#ffffff"
+                    className="bg-white/30 backdrop-invert backdrop-opacity-10 p-1 rounded-lg"
+                />
+                <div className="flex justify-center yellow p-5 stars">
+                    <Star fill="yellow" />
+                    <Star fill="yellow" />
+                    <Star fill="yellow" />
+                    <Star fill="yellow" />
+                    <Star fill="yellow" />
+                </div>
+                <p className="p-3 text-xl">{p}</p>
+                <p className="text-2xl">{emoji}</p>
+                <h6 className="font-bold text-2xl">{name}</h6>
+                <p className="grey">{title}</p>
+                <h6>{record}</h6>
+                <p>{description}</p>
             </div>
-            <p className="p-3 text-xl">{p}</p>
-            <p className="text-2xl">{emoji}</p>
-            <h6 className="font-bold text-2xl">{name}</h6>
-            <p className="grey">{title}</p>
-            <h6>{record}</h6>
-            <p>{description}</p>
         </MotionFadeIn>
     );
 };
